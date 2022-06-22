5a788fed9f939642e2fee1a20d9162ba3151c70e
Shirali <shiralialihummatov@gmail.com>
2022-06-22 10:33:46 +0400

    WebApp

--- FutuREsumeWebApp/src/main/java/com/company/futuresumewebapp/MyServletPage.java
+++ FutuREsumeWebApp/src/main/java/com/company/futuresumewebapp/MyServletPage.java
@@ -7,6 +7,7 @@ package com.company.futuresumewebapp;
 import com.company.dao.inter.UserDaoInter;
 import com.company.entity.User;
 import com.company.main.Context;
+import org.graalvm.compiler.lir.LIRInstruction;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -21,31 +22,40 @@ import java.util.List;
  *
  * @author Shireli
  */
-@WebServlet(name="MyServletPage", urlPatterns = "/MyServletPage")
+@WebServlet(name = "MyServletPage", urlPatterns = "/MyServletPage")
 public class MyServletPage extends HttpServlet {
+
     private UserDaoInter userDao = Context.instanceUserDao();
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
         List<User> users = userDao.getAll();
-        try (PrintWriter out = response.getWriter()){
+        try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
             out.println("<title>MyServletPage</title>");
             out.println("</head>");
             out.println("<body>");
-            for (User u: users) {
+            out.println("I got GET request");
+            for (User u : users) {
                 out.println(u);
             }
             out.println("</body>");
             out.println("</html>");
         }
     }
+
+    @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String name = String.valueOf(request.getAttribute("name"));
+        String surname = String.valueOf(request.getAttribute("surname"));
+
+        User u = new User(0, name, surname, null, null, null, null, null, null, null);
+        boolean users = userDao.addUser(u);
         response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()){
+        try (PrintWriter out = response.getWriter()) {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
@@ -53,6 +63,7 @@ public class MyServletPage extends HttpServlet {
             out.println("</head>");
             out.println("<body>");
             out.println("I got POST request");
+            out.println("User inserted: " + u.getId() + "<br>");
             out.println("</body>");
             out.println("</html>");
         }
@@ -65,4 +76,6 @@ public class MyServletPage extends HttpServlet {
     protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
+
+    pub
 }
