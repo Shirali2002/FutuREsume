700ae5314e02a848857588f3de306f4f7cc55bb3
Shirali <shiralialihummatov@gmail.com>
2022-06-22 10:33:18 +0400

    DbApp

--- FutureResumeDbApp/src/main/java/com/company/dao/inter/AbstractDao.java
+++ FutureResumeDbApp/src/main/java/com/company/dao/inter/AbstractDao.java
@@ -4,13 +4,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public abstract class AbstractDao {
-
+    
     public Connection connection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
         String url = "jdbc:mysql://localhost:3306/futureresumedb";
         String username = "root";
         String password = "Joker02";
         Connection c = DriverManager.getConnection(url, username, password);
         return c;
     }
-
+    
 }
